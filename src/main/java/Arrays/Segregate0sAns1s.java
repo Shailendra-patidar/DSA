1d2d1ab411b2a8e22b839817a68ade7f435c2f78
Shailendra patidar <shelualot123@gmail.com>
2026-05-17 13:38:56 +0530

    separate 0s and 1s

---
+++ src/main/java/Arrays/Segregate0sAns1s.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+public class Segregate0sAns1s {
+    public static void main(String[] args) {
+        int[] arr = {0,1,0,1,1,0};
+        int numberOfZeros = 0;
+        int numberOfOnes = 0;
+        for (int ele : arr){
+            if (ele == 0){
+                numberOfZeros++;
+            }
+            else numberOfOnes++;
+        }
+        System.out.print("Before segregation: ");
+        for (int ele: arr){
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+        for (int i = 0; i < numberOfZeros; i++) {
+            arr[i] = 0;
+        }
+        for (int i = numberOfZeros; i < arr.length; i++) {
+            arr[i] = 1;
+        }
+        System.out.print("After segregation: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
