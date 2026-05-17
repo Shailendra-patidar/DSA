5b3f8fa23433ab95e2e0369d55e57858cf8b7e4c
Shailendra patidar <shelualot123@gmail.com>
2026-05-17 14:55:17 +0530

    separate 0s and 1s using two pointer technique

---
+++ src/main/java/Arrays/segregate0sAnd1sUsingTwoPointer.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+public class segregate0sAnd1sUsingTwoPointer {
+    public static void main(String[] args) {
+        int[] arr = {0,1,1,0,0,1,0,1,0,0,1};
+        int n = arr.length;
+        int i=0;
+        int j = n-1;
+        while(i<j){
+            if (arr[i]==1 && arr[j]==0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+
+            } else if (arr[i]==0 && arr[j]==0) {
+                i++;
+            } else if (arr[i]==1 && arr[j]==1) {
+                j--;
+            }
+            else {
+                i++;
+                j--;
+            }
+        }
+        for (int e : arr){
+            System.out.print(e+" ");
+        }
+    }
+}
