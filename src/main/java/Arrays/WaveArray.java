f2f05118e825de5bd8ebc45b44d7179d6ef5f7a6
Shailendra patidar <shelualot123@gmail.com>
2026-05-17 15:22:24 +0530

    wave array

---
+++ src/main/java/Arrays/WaveArray.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class WaveArray {
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,7,8,9};
+        for (int i = 0; i < arr.length-1; i+=2) {
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
+        for (int e : arr){{
+            System.out.print(e+" ");
+        }}
+    }
+}
