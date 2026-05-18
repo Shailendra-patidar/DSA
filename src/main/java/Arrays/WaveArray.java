3db2c4225ed320af9c331289605c7a6b792022de
Shailendra patidar <shelualot123@gmail.com>
2026-05-18 21:09:16 +0530

    No change

--- src/main/java/Arrays/WaveArray.java
+++ src/main/java/Arrays/WaveArray.java
@@ -2,7 +2,7 @@ package Arrays;
 
 public class WaveArray {
     public static void main(String[] args) {
-        int[] arr = {2,4,6,7,8,9};
+        int[] arr = {2,4,6,7,8};
         for (int i = 0; i < arr.length-1; i+=2) {
             int temp = arr[i];
             arr[i] = arr[i+1];
