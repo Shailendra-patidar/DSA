a577c853c5494153bdf9f8833c0853e53dcb4cee
Shailendra patidar <shelualot123@gmail.com>
2026-05-27 08:57:04 +0530

    Approach 2 (using single for loop)

--- src/main/java/Arrays/Question2.java
+++ src/main/java/Arrays/Question2.java
@@ -2,15 +2,30 @@ package Arrays;
 
 public class Question2 {
     public static void main(String[] args) {
-        int[] x = {2,5,6,2,9};
-        int k = 0;
-        for (int i = 0; i < x.length-1; i++) {
-            for (int j = i+1; j < x.length; j++) {
-                if(x[i] == x[j]){
-                    k = x[i];
-                }
-            }
-        }
-        System.out.println("Duplicate element is: "+k);
+        int[] x = {2,5,6,5,9};
+//        int k = x[0];
+        //Approach 1
+//        for (int i = 0; i < x.length-1; i++) {
+//            for (int j = i+1; j < x.length; j++) {
+//                if(x[i] == x[j]){
+//                    k = x[i];
+//                }
+//            }
+//        }
+//        System.out.println("Duplicate element is: "+k);
+
+        //Approach 2
+//        for (int i = 0; i < x.length-1; i++) {
+//            if (x[i+1] == k){
+//                k = x[i+1];
+//                break;
+//            }
+//            else{
+//                k = x[i];
+//            }
+//        }
+//        System.out.println("Duplicate element is: "+ k);
+
+
     }
 }
