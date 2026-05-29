9fb4ef535e9d1cf19a91318efc28fa9e5414eb87
Shailendra patidar <shelualot123@gmail.com>
2026-05-29 12:27:18 +0530

    optimized Bubble sort

--- src/main/java/Sorting/BubbleSort.java
+++ src/main/java/Sorting/BubbleSort.java
@@ -4,14 +4,20 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {12,2,4,6,5,4,0,8};
         for (int i = 0; i < arr.length-1; i++) {
+            int isSorted = 0;
             for (int j = 0; j < arr.length-1-i; j++) {
                 if(arr[j]>arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    isSorted++;
                 }
             }
+            if (isSorted == 0){
+                break;
+            }
         }
+        //Sorted array
         for(int ele : arr){
             System.out.print(ele+" ");
         }
