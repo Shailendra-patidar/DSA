5cf30746bb046a27378ef7f06a23a7044ad98971
Shailendra patidar <shelualot123@gmail.com>
2026-05-31 07:44:24 +0530

    Bubble Sort

---
+++ src/main/java/GFGProblems/BubbleSort.java
@@ -0,0 +1,18 @@
+package GFGProblems;
+
+public class BubbleSort {
+    class Solution {
+        public void bubbleSort(int[] arr) {
+            // code here
+            for(int i = 0; i< arr.length;i++){
+                for(int j = 0; j<arr.length-1-i;j++){
+                    if(arr[j]>arr[j+1]){
+                        arr[j] = arr[j] + arr[j+1];
+                        arr[j+1] = arr[j] - arr[j+1];
+                        arr[j] = arr[j] - arr[j+1];
+                    }
+                }
+            }
+        }
+    }
+}
