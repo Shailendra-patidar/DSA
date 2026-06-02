2eaa317995ff21b2fdb1f889514c398ee42e4d26
Shailendra patidar <shelualot123@gmail.com>
2026-06-02 15:49:54 +0530

    Insertion sort

---
+++ src/main/java/GFGProblems/InsertionSort.java
@@ -0,0 +1,21 @@
+package GFGProblems;
+
+public class InsertionSort {
+    class Solution {
+        // Please change the array in-place
+        public void insertionSort(int arr[]) {
+            // code here
+            for(int i = 0; i<arr.length;i++){
+                int j = i;
+                while(j>0 && arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    j--;
+                }
+
+            }
+
+        }
+    }
+}
