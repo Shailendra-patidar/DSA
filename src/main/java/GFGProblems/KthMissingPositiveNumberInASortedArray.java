70a7c650496d77f32039eb36ddd7066e78929df2
Shailendra patidar <shelualot123@gmail.com>
2026-06-07 07:43:07 +0530

    Kth Missing Positive Number In A Sorted Array

---
+++ src/main/java/GFGProblems/KthMissingPositiveNumberInASortedArray.java
@@ -0,0 +1,18 @@
+package GFGProblems;
+
+public class KthMissingPositiveNumberInASortedArray {
+    class Solution {
+        public int kthMissing(int[] arr, int k) {
+            // code here
+            int lo = 0, hi = arr.length-1;
+            while(lo<=hi){
+                int mid = (lo + hi) / 2;
+                int correctNo = mid + 1;
+                int missing = arr[mid] - correctNo;
+                if(missing >= k) hi = mid - 1;
+                else lo = mid + 1;
+            }
+            return lo + k;
+        }
+    }
+}
