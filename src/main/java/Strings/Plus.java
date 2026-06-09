fb50ac22b87e4b37705c389cb43d74242aa03eaa
Shailendra patidar <shelualot123@gmail.com>
2026-06-09 17:50:35 +0530

    behaviour of plus in string

---
+++ src/main/java/Strings/Plus.java
@@ -0,0 +1,19 @@
+package Strings;
+
+public class Plus {
+    public static void main(String[] args) {
+//        String s = "bhakti";
+//        s = s + " hi shakti he";
+//        s+= 10;
+//        s+= 'a';
+//        s+='\n';
+//        s+='h';
+//        System.out.println(s);
+        System.out.println("shailendra"+10+20);
+        System.out.println(10+20+"shailendra");
+        System.out.println("shailendra"+(10+20));
+        System.out.println('A'+ 10 + "shailendra"); // 'A' Ascii vlaue = 65
+    }
+
+
+}
