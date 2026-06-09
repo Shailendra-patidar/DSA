0348d9b213e75b4b13ecdfe63fb8fd9046e033fd
Shailendra patidar <shelualot123@gmail.com>
2026-06-09 18:33:21 +0530

    string to array

---
+++ src/main/java/Strings/StringToCharArray.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class StringToCharArray {
+    public static void main(String[] args) {
+        String str = "Ram";
+        char[] arr = str.toCharArray();
+        for(char e : arr){
+            System.out.print(e);
+        }
+    }
+}
