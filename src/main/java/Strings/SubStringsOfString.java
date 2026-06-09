6cd0e8a5e39c2edeb57d38684966a9705559e76c
Shailendra patidar <shelualot123@gmail.com>
2026-06-09 18:33:41 +0530

    substring method

---
+++ src/main/java/Strings/SubStringsOfString.java
@@ -0,0 +1,8 @@
+package Strings;
+
+public class SubStringsOfString {
+    public static void main(String[] args) {
+        String str = "mahadev";
+        System.out.println(str.substring(4, str.length()));
+    }
+}
