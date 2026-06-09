6fb94cb58ab8df42b65b03d4b106ac4adb891b8b
Shailendra patidar <shelualot123@gmail.com>
2026-06-09 16:48:29 +0530

    understanding toString()

---
+++ src/main/java/Strings/CompareToStringMethod.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class CompareToStringMethod {
+    public static void main(String[] args) {
+        String a = "shailendra";
+        String b = "vaibhav";
+        System.out.println(a.compareTo(b));
+    }
+}
