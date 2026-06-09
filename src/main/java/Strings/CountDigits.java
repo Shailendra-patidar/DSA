b8f0040079c8663952834bf10925ca825d9709f1
Shailendra patidar <shelualot123@gmail.com>
2026-06-09 17:50:06 +0530

    count digits

---
+++ src/main/java/Strings/CountDigits.java
@@ -0,0 +1,16 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        String str = ""+n;
+//        System.out.println(str.length());
+
+        double n = sc.nextDouble();
+        String s = "" + n;
+        System.out.println(s.length());
+    }
+}
