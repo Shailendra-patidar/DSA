f6429a7c0b88a1c052ea73118dd2f2965459a72b
Shailendra patidar <shelualot123@gmail.com>
2026-06-11 21:44:31 +0530

    mostFrequentCharacter(brute force)

---
+++ src/main/java/GFGProblems/MostFrequentCharacter.java
@@ -0,0 +1,28 @@
+package GFGProblems;
+
+public class MostFrequentCharacter {
+    // method 1 brute force
+    class Solution {
+        public char getMaxOccuringChar(String s) {
+            // code here
+            int n = s.length();
+            int maxFreq = -1;
+            char ans = s.charAt(0);
+            for(int i = 0;i<n;i++){
+                int freq = 1;
+                char ch = s.charAt(i);
+                for(int j = i+1;j<n;j++){
+                    if(s.charAt(j) == ch) freq++;
+                }
+                if(freq>maxFreq){
+                    maxFreq = freq;
+                    ans = ch;
+                }
+                else if(freq==maxFreq && ch<ans){
+                    ans = ch;
+                }
+            }
+            return ans;
+        }
+    }
+}
