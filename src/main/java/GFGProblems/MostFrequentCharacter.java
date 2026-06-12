79d805c4f2c9738c2f86555ecadc8bdedee819e0
Shailendra patidar <shelualot123@gmail.com>
2026-06-12 21:18:20 +0530

    mostFrequentCharacter(optimized method)

--- src/main/java/GFGProblems/MostFrequentCharacter.java
+++ src/main/java/GFGProblems/MostFrequentCharacter.java
@@ -1,28 +1,47 @@
 package GFGProblems;
 
 public class MostFrequentCharacter {
-    // method 1 brute force
+
     class Solution {
         public char getMaxOccuringChar(String s) {
+            // method 1 brute force
             // code here
+//            int n = s.length();
+//            int maxFreq = -1;
+//            char ans = s.charAt(0);
+//            for(int i = 0;i<n;i++){
+//                int freq = 1;
+//                char ch = s.charAt(i);
+//                for(int j = i+1;j<n;j++){
+//                    if(s.charAt(j) == ch) freq++;
+//                }
+//                if(freq>maxFreq){
+//                    maxFreq = freq;
+//                    ans = ch;
+//                }
+//                else if(freq==maxFreq && ch<ans){
+//                    ans = ch;
+//                }
+//            }
+//            return ans;
+
+            //method 2;
             int n = s.length();
-            int maxFreq = -1;
-            char ans = s.charAt(0);
+            int[] freq = new int[26];
             for(int i = 0;i<n;i++){
-                int freq = 1;
                 char ch = s.charAt(i);
-                for(int j = i+1;j<n;j++){
-                    if(s.charAt(j) == ch) freq++;
-                }
-                if(freq>maxFreq){
-                    maxFreq = freq;
-                    ans = ch;
-                }
-                else if(freq==maxFreq && ch<ans){
-                    ans = ch;
+                int idx = ch-'a';
+                freq[idx]++;
+            }
+            int maxFreq = 0;
+            char ans = s.charAt(0);
+            for(int i = 0;i<26;i++){
+                if(freq[i]>maxFreq){
+                    maxFreq = freq[i];
+                    ans = (char)(i+97);
                 }
             }
-            return ans;
+            return  ans;
         }
     }
 }
