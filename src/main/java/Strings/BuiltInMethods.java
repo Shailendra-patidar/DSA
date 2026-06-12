431dcdc9a68d7ef630a6719612ee11ff1d692d9c
Shailendra patidar <shelualot123@gmail.com>
2026-06-12 21:18:44 +0530

    Basic inBuilt method in string

---
+++ src/main/java/Strings/BuiltInMethods.java
@@ -0,0 +1,22 @@
+package Strings;
+
+public class BuiltInMethods {
+    public static void main(String[] args) {
+//        String str = "Shailendra patidar";
+//        System.out.println(str.indexOf('a'));  // first occurrence of 'a'
+//        System.out.println(str.lastIndexOf('a')); // last occurrence of 'a'
+//        String s = "harshita";
+//        System.out.println(s.contains("harsh"));
+//        if (s.contains("harshit")){
+//            System.out.println("Happy");
+//        }
+//        System.out.println(s.startsWith("har"));
+//        System.out.println(s.endsWith("mak"));
+//        String a = " patidar";
+//        System.out.println(s.concat(a));
+
+        String str = "370";
+        int n = Integer.parseInt(str);
+        System.out.println(n+5);
+    }
+}
