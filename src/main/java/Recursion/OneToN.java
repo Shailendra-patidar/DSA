cbd456a5a12d19fa57015dfd167c1aac33db9750
Shailendra patidar <shelualot123@gmail.com>
2026-06-23 13:50:02 +0530

    printing 1 to n numbers using recursion

---
+++ src/main/java/Recursion/OneToN.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class OneToN {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        print(1,n);
+//    }
+//    public static void print(int x, int n){
+//        if(x>n) return;
+//        System.out.println(x);
+//        print(x+1,n);
+//    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        print(n);
+    }
+    public static void print(int n){
+        if(n==0) return;
+        print(n-1);
+        System.out.print(n+" ");
+    }
+
+}
