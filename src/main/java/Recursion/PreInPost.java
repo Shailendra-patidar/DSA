1c145e84b00e4d5368beea233d13c2e17e7c250a
Shailendra patidar <shelualot123@gmail.com>
2026-07-08 07:15:42 +0530

    pre in post

---
+++ src/main/java/Recursion/PreInPost.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class PreInPost {
+    public static void main(String[] args) {
+        pip(3);
+    }
+
+    private static void pip(int n) {
+        if(n==0) return;
+        System.out.println(n+" ");
+        pip(n-1);
+        System.out.println(n+" ");
+        pip(n-1);
+        System.out.println(n+" ");
+    }
+}
