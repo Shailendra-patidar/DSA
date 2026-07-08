4837e13047f2b70e443a736a4f832c38840f3b02
Shailendra patidar <shelualot123@gmail.com>
2026-07-08 10:41:55 +0530

    merge sort

---
+++ src/main/java/Sorting/MergeSort.java
@@ -0,0 +1,39 @@
+package Sorting;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {2,4,7,1,0,6,3,-1};
+        mergeSort(arr);
+        for(int ele: arr){
+            System.out.print(ele+" ");
+        }
+    }
+
+    public static void mergeSort(int[] arr){
+        int n = arr.length;
+        if(n==1) return;
+        int[] a = new int[n/2];
+        int[] b = new int[n-n/2];
+        int idx = 0;
+        for (int i = 0; i < a.length; i++) {
+            a[i] = arr[idx++];
+        }
+        for (int i = 0; i < b.length; i++) {
+            b[i] = arr[idx++];
+        }
+        mergeSort(a);
+        mergeSort(b);
+        merge(a,b,arr);
+    }
+
+    public static void merge(int[] a, int[] b, int[] c){
+        int i=0, j = 0,k=0;
+        while(i<a.length && j<b.length){
+            if(a[i]<b[j]) c[k++] = a[i++];
+            else c[k++] = b[j++];
+        }
+        while (i<a.length) c[k++] = a[i++];
+        while(j<b.length) c[k++] = b[j++];
+    }
+
+}
